b95b7fd5c1a36ff6f28889f946072ca14f727988
Kale Champagnie <42823429+oelin@users.noreply.github.com>
2023-10-08 20:03:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 export function assert(value) {
-  if (value !== true) throw new Error('Assertion failed!')
+    if (value !== true) throw new Error('Assertion failed!')
 }
 
 export function Validated(Type, validator) {
-  
-	return function ValidatedType(...arguments) {
-    const instance = new Type(...arguments)
-    
-		return assert(validator(instance))  // Validation.
-    return Object.freeze(instance)      // Immutability.
-	}
+
+    return function ValidatedType(...arguments) {
+        const instance = new Type(...arguments)
+
+        return assert(validator(instance)) // Validation.
+        return Object.freeze(instance) // Immutability.
+    }
 }
